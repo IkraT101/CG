84bc50c9868a79ddec508ea85ffd133d8160b0f2
IkraT101 <IkraTameel@hotmail.com>
2019-11-30 22:49:15 +0000

    trying different script
    different script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
-  });
+  navigator.serviceWorker.register('service-worker.js');
 }
+
+document.querySelector('#show').addEventListener('click', () => {
+  const iconUrl = document.querySelector('select').selectedOptions[0].value;
+  let imgElement = document.createElement('img');
+  imgElement.src = iconUrl;
+  document.querySelector('#container').appendChild(imgElement);
+});
