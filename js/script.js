d88cce7f09e9f3165ecf47e27d3a4748018bdab6
IkraT101 <IkraTameel@hotmail.com>
2019-11-30 22:50:17 +0000

    going backwards
    deleated newer script and set to old

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js');
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
 }
-
-document.querySelector('#show').addEventListener('click', () => {
-  const iconUrl = document.querySelector('select').selectedOptions[0].value;
-  let imgElement = document.createElement('img');
-  imgElement.src = iconUrl;
-  document.querySelector('#container').appendChild(imgElement);
-});
