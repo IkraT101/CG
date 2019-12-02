aadcbd83461a8398d3e2ef5f808596986b92859f
IkraT101 <IkraTameel@hotmail.com>
2019-12-02 21:01:38 +0000

    new pages
    more pages

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
-if('serviceWorker' in navigator){
-navigator.serviceWorker.register('/sw.js')
-.then((reg) => console.log('service worker registered', reg))
-.catch((err) => console.log('service worker not registered', err));
+//if('serviceWorker' in navigator){
+//navigator.serviceWorker.register('/sw.js')
+//.then((reg) => console.log('service worker registered', reg))
+//.catch((err) => console.log('service worker not registered', err));
+//}
+
+
+// if ('serviceWorker' in navigator) {
+//  window.addEventListener('load', function() {
+  //  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+    //.then((ref) => console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    //}, function(err) {
+      // registration failed :(
+    //.catch((err)) => console.log('ServiceWorker registration failed: ', err);
+    //});
+  //});
+//}
+//navigator.serviceWorker.register('/sw.js', {
+  //scope: '/'
+//});
+
+
+if('serviceWorker' in navigator) {
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register('/sw.js').then(function() {
+    console.log('CLIENT: service worker registration complete.');
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
 }
