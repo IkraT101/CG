c4df31f76f06ccb2961aa3b27ab2c2fe735f86d6
IkraT101 <IkraTameel@hotmail.com>
2019-12-03 20:33:35 +0000

    sign in attempt 1
    adding google sign in

--- js/script.js
+++ js/script.js
@@ -41,6 +41,24 @@ if ('serviceWorker' in navigator) {
       console.log('ServiceWorker registration failed: ', err);
     });
   });
-}  
+}
 
 console.log('Hello from service-worker.js');
+
+
+<meta name="google-signin-client_id" content="90282911210-qmmf4muu6de08ogh5tn0qrm3e0jj78an.apps.googleusercontent.com">
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+<a href="#" onclick="signOut();">Sign out</a>
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
