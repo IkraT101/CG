70528df2a7f2f304875e7e4ba84159ccdba90ecf
IkraT101 <IkraTameel@hotmail.com>
2019-12-03 19:22:09 +0000

    Update script.js
    Tried a different serviceworker script.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
-if('serviceWorker' in navigator){
-navigator.serviceWorker.register('sw.js')
-.then((reg) => console.log('service worker registered', reg))
-.catch((err) => console.log('service worker not registered', err));
+// if('serviceWorker' in navigator){
+// navigator.serviceWorker.register('sw.js')
+// .then((reg) => console.log('service worker registered', reg))
+// .catch((err) => console.log('service worker not registered', err));
+// }
+
+
+if ('serviceWorker' in navigator) {
+ window.addEventListener('load', function() {
+   navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      Registration was successful
+    .then((ref) => console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      registration failed :(
+    .catch((err)) => console.log('ServiceWorker registration failed: ', err);
+    });
+  });
 }
-
-
-// if ('serviceWorker' in navigator) {
-//  window.addEventListener('load', function() {
-  //  navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-    //.then((ref) => console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    //}, function(err) {
-      // registration failed :(
-    //.catch((err)) => console.log('ServiceWorker registration failed: ', err);
-    //});
-  //});
-//}
-//navigator.serviceWorker.register('/sw.js', {
-  //scope: '/'
-//});\
+navigator.serviceWorker.register('/sw.js', {
+  scope: '/'
+});
 
 
 //if('serviceWorker' in navigator) {
