321a9d9366d4a5fb8f0029ecce8491ab4b24a211
IkraT101 <IkraTameel@hotmail.com>
2019-12-03 19:23:55 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
 
 if ('serviceWorker' in navigator) {
  window.addEventListener('load', function() {
-   navigator.serviceWorker.register('sw.js').then(function(registration) {
+   navigator.serviceWorker.register('/js/sw.js').then(function(registration) {
       Registration was successful
     .then((ref) => console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
