06176d39971a611c0f59b0d42d5ea3a6511cfb39
IkraT101 <IkraTameel@hotmail.com>
2019-12-06 12:24:27 +0000

    grid in gallary
    added more css

--- js/script.js
+++ js/script.js
@@ -1,36 +1,94 @@
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function() {
+//     navigator.serviceWorker.register('/sw.js').then(function(registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function(err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+//   });
+// }
 
 console.log('Hello from service-worker.js');
 
-// <meta name="google-signin-client_id" content="90282911210-qmmf4muu6de08ogh5tn0qrm3e0jj78an.apps.googleusercontent.com">
-//
-// function onSignIn(googleUser) {
-//   var profile = googleUser.getBasicProfile();
-//   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-//   console.log('Name: ' + profile.getName());
-//   console.log('Image URL: ' + profile.getImageUrl());
-//   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-// }
-// <a href="#" onclick="signOut();">Sign out</a>
-//   function signOut() {
-//     var auth2 = gapi.auth2.getAuthInstance();
-//     auth2.signOut().then(function () {
-//       console.log('User signed out.');
-//     });
-//   }
 
 
 $('#myCarousel').carousel({
   interval: 4000
 })
+
+
+var grid = new Muuri('.grid');
+var grid = new Muuri('.grid', {dragEnabled: true});
+var grid = new Muuri(elem, {
+  layout: {
+    fillGaps: true,
+    horizontal: true,
+    alignRight: true,
+    alignBottom: true,
+    rounding: false
+  }
+});
+
+//
+// // Use specific items.
+// var grid = new Muuri(elem, {
+//   items: [elemA, elemB, elemC]
+// });
+//
+// // Use node list.
+// var grid = new Muuri(elem, {
+//   items: elem.querySelectorAll('.item')
+// });
+//
+// // Use selector.
+// var grid = new Muuri(elem, {
+//   items: '.item'
+// });
+
+
+//
+// // Build your own layout algorithm.
+// var grid = new Muuri(elem, {
+//   layout: function (items, gridWidth, gridHeight) {
+//     // The layout data object. Muuri will read this data and position the items
+//     // based on it.
+//     var layout = {
+//       // The layout's item slots (left/top coordinates).
+//       slots: [],
+//       // The layout's total width.
+//       width: 0,
+//       // The layout's total height.
+//       height: 0,
+//       // Should Muuri set the grid's width after layout?
+//       setWidth: true,
+//       // Should Muuri set the grid's height after layout?
+//       setHeight: true
+//     };
+//
+//     // Calculate the slots.
+//     var item;
+//     var m;
+//     var x = 0;
+//     var y = 0;
+//     var w = 0;
+//     var h = 0;
+//     for (var i = 0; i < items.length; i++) {
+//       item = items[i];
+//       x += w;
+//       y += h;
+//       m = item.getMargin();
+//       w = item.getWidth() + m.left + m.right;
+//       h = item.getHeight() + m.top + m.bottom;
+//       layout.slots.push(x, y);
+//     }
+//
+//     // Calculate the layout's total width and height.
+//     layout.width = x + w;
+//     layout.height = y + h;
+//
+//     return layout;
+//   }
+// });
